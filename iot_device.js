e394b9ff260500450740725a892ca05a077f4549
chewingbytes <bryanchewzy24@gmail.com>
2026-02-05 21:43:44 +0800

    iniitail

--- iot_device.js
+++ iot_device.js
@@ -1,34 +1,36 @@
 var awsIoT = require('aws-iot-device-sdk');
 
 var device = awsIoT.device({
-    keyPath: "./certs/46da3bb2f657600bfba45ec93cfdde27c2a7c8c4ae18724a014d4f3450c5cfbb-private.pem.key",
-    certPath: "./certs/46da3bb2f657600bfba45ec93cfdde27c2a7c8c4ae18724a014d4f3450c5cfbb-certificate.pem.crt",
+    keyPath: "./certs/private-pem.key",
+    certPath: "./certs/certificate.pem.crt",
     caPath: './certs/AmazonRootCA1.pem',
     clientId: 'first-try',
-        host: 'avvyh2o0cz7xw-ats.iot.us-east-1.amazonaws.com'
+        host: 'a3cgwqvwz0nizh-ats.iot.ap-southeast-1.amazonaws.com'
 })
+
+const now = new Date();
 
 const sampleData = [
   {
     device_id: 1,
+    time: new Date(now.getTime() - 2000).toISOString(),
     current: 0.85,
     voltage: 220,
-    power: 1500,
-    timestamp: new Date().toISOString()
+    power: 1500
   },
   {
     device_id: 2,
+    time: new Date(now.getTime() - 1000).toISOString(),
     current: 1.20,
     voltage: 220,
-    power: 9999,
-    timestamp: new Date().toISOString()
+    power: 9999
   },
   {
     device_id: 3,
+    time: new Date(now.getTime()).toISOString(),
     current: 0.95,
     voltage: 220,
-    power: 209.0,
-    timestamp: new Date().toISOString()
+    power: 209.0
   }
 ];
 
@@ -40,8 +42,20 @@ device.on("connect", function () {
         setTimeout(() => {
             console.log("Publishing Data:", data);
             device.publish("Energy_Usage", JSON.stringify(data));
-        }, index * 3000); // Publish every 3 seconds
+        }, index * 3000); 
     });
+});
+
+device.on("error", function (err) {
+  console.error("AWS IoT Error:", err);
+});
+
+device.on("reconnect", function () {
+  console.log("Reconnecting to AWS IoT...");
+});
+
+device.on("offline", function () {
+  console.log("AWS IoT Offline");
 });
 
 device.on("message", function(topic, payload){
